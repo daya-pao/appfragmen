2409c466c7c232f81d7a4b2b0e310923c0084049
daya-pao <dayana06052002@gmail.com>
2024-03-11 18:25:17 -0500

    practica de fragmentos en Kotlin-2

--- app/src/main/java/com/example/appfragmentos/BlankFragment2.kt
+++ app/src/main/java/com/example/appfragmentos/BlankFragment2.kt
@@ -5,12 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.os.bundleOf
 
 class BlankFragment2 : Fragment(R.layout.fragment_blank2) {
 
+    private var nombre:String?=""
+    private var edad:Int = 0
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        var datos = view.findViewById<TextView>(R.id.txtDatos)
+
+        datos.text = "su nombre es: $nombre y su edad es: $edad"
+
     }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState
+        )
+
+        nombre=requireArguments().getString(MI_NOMBRE)
+        edad=requireArguments().getInt(MI_EDAD)
+
+    }
+
+
+    companion object{
+        private const val MI_NOMBRE="nombre"
+        private const val MI_EDAD= "edad"
+
+        fun newInstance(nombre:String , edad:Int)= BlankFragment2().apply {
+            arguments = bundleOf(MI_NOMBRE to nombre , MI_EDAD to edad) }
+    }
+
+
 
 }
